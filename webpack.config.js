66552296bbb8a751cbbacedd0af2941fa343068e
requezz <muzagov95@mail.ru>
2024-01-14 15:21:43 +0300

    Finish table resize

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,7 @@ module.exports = (env, argv) => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
             },
           },
         },
